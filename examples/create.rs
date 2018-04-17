9047a54a91a9a77802336a84fbeb21ff080c658c
elast0ny <elast0ny00@gmail.com>
2018-04-17 16:13:43 -0400

    Taking write lock in a more elegant way. Also writes strings now.

--- examples/create.rs
+++ examples/create.rs
@@ -5,18 +5,30 @@ use std::path::PathBuf;
 
 fn main() {
 
-    let my_path: PathBuf = PathBuf::from("test.txt");
-    let my_perms: MemPermission = MemPermission {read:true, write:true, execute:false};
-
     //Create a new RW- MemFile of size 4096
-    let mut mem_file: MemFile = match MemFile::create(my_path.clone(), my_perms, 4096) {
+    let mut mem_file: MemFile = match MemFile::create(PathBuf::from("test.txt"), 4096) {
         Ok(v) => v,
         Err(e) => {
             println!("Error : {}", e);
-            println!("Failed to create MemFile at \"{}\"...", my_path.to_string_lossy());
+            println!("Failed to create MemFile...");
             return;
         }
     };
+
+    {
+        let mut write_buf: MemFileWLockSlice<char> = match mem_file.wlock_as_slice() {
+            Ok(v) => v,
+            Err(_) => panic!("Failed to acquire write lock !"),
+        };
+
+        write_buf[0] = 'H';
+        write_buf[1] = 'e';
+        write_buf[2] = 'l';
+        write_buf[3] = 'l';
+        write_buf[4] = 'o';
+        write_buf[5] = '\x00';
+        write_buf[6] = '\x00';
+    }
 
     println!("Waiting for first byte of shared memory to change...");
 
@@ -24,19 +36,19 @@ fn main() {
     loop {
 
         //Acquire read lock
-        let shared_mem: &[u8] = match mem_file.rlock_as_slice() {
-            Ok(v) => *v,
+        let read_buf: MemFileRLockSlice<char> = match mem_file.rlock_as_slice() {
+            Ok(v) => v,
             Err(_) => panic!("Failed to acquire write lock !"),
         };
 
-        //Read shared memory
-        if shared_mem[0] == 0x1 {
+        //Check shared memory
+        if read_buf[0] != 'H' {
             //This will also drop the lock
             break;
         }
 
         //Release the lock before sleeping
-        drop(shared_mem);
+        drop(read_buf);
         std::thread::sleep(std::time::Duration::from_millis(200));
     }
 
@@ -44,23 +56,17 @@ fn main() {
 
     //Modify the shared memory just for fun
     {
-        /* TODO : Make this work somehow
-        let buffer: &mut [u8] = match mem_file.wlock_as_slice() {
-            Ok(v) => &mut *v,
-            Err(_) => return,
-        };
-        */
-
-        let mut wlock = match mem_file.wlock_as_slice::<char>() {
+        let mut write_buf: MemFileWLockSlice<char> = match mem_file.wlock_as_slice() {
             Ok(v) => v,
             Err(_) => panic!("Failed to acquire write lock !"),
         };
 
-        let write_buf: &mut [char] = &mut *wlock;
-
         write_buf[0] = 'B';
         write_buf[1] = 'y';
         write_buf[2] = 'e';
+
+        write_buf[3] = '\x00';
+        write_buf[4] = '\x00';
     }
 
     println!("Done !");
--- examples/open.rs
+++ examples/open.rs
@@ -4,54 +4,50 @@ use std::path::PathBuf;
 
 fn main() {
 
-    let my_path: PathBuf = PathBuf::from("test.txt");
-    let my_perms: MemPermission = MemPermission {
-        read:true,
-        write:true,
-        execute:true};
-
-    let mut mem_file: MemFile = match MemFile::open(my_path.clone(), my_perms) {
+    //Open an existing MemFile
+    let mut mem_file: MemFile = match MemFile::open(PathBuf::from("test.txt")) {
         Ok(v) => v,
         Err(e) => {
             println!("Error : {}", e);
-            println!("Failed to open \"{}\"", my_path.to_string_lossy());
+            println!("Failed to open MemFile...");
             return;
         }
     };
 
+    //Read the original contents
     {
-        let buffer: &[u8] = match mem_file.rlock_as_slice() {
-            Ok(v) => *v,
-            Err(_) => return,
-        };
-
-        print!("Orig buffer = \"");
-        for b in &buffer[0..16] {
-            print!("\\x{:02x}", b);
-        }
-        println!("\"");
-    }
-
-    {
-        let mut wlock = match mem_file.wlock_as_slice::<u8>() {
+        //Acquire read lock
+        let read_buf: MemFileRLockSlice<u8> = match mem_file.rlock_as_slice() {
             Ok(v) => v,
             Err(_) => panic!("Failed to acquire write lock !"),
         };
-        let write_buf: &mut [u8] = &mut *wlock;
+
+        print!("Orig buffer = \"");
+        print!("{}", unsafe {std::str::from_utf8_unchecked(*read_buf)});
+        println!("\"");
+    }
+
+    //Write 0x01 in the first byte
+    {
+        let mut write_buf: MemFileWLockSlice<u8> = match mem_file.wlock_as_slice() {
+            Ok(v) => v,
+            Err(_) => panic!("Failed to acquire write lock !"),
+        };
+
         println!("write_buf[0] = 0x1");
         write_buf[0] = 0x1;
     }
 
+    //Read the contents of the buffer again
     std::thread::sleep(std::time::Duration::from_secs(1));
-
     {
         let buffer = match mem_file.rlock_as_slice::<u8>() {
-            Ok(v) => *v,
-            Err(_) => return,
+            Ok(v) => v,
+            Err(_) => panic!("Failed to acquire write lock !"),
         };
 
         print!("After buffer = \"");
-        print!("{}", unsafe {std::str::from_utf8_unchecked(buffer)});
+        print!("{}", unsafe {std::str::from_utf8_unchecked(*buffer)});
         println!("\"");
     }
 
