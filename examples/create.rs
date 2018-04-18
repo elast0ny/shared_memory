1948696ac0c14e56762dca73bb53784e36f23a63
elast0ny <elast0ny00@gmail.com>
2018-04-18 10:41:16 -0400

    Now use a mix of regular locks and slice locks.

--- examples/create.rs
+++ examples/create.rs
@@ -2,9 +2,16 @@ extern crate mem_file;
 use mem_file::*;
 use std::path::PathBuf;
 
+struct SharedState {
+    num_listenners: u32,
+    message: [u8; 256],
+}
+//WARNING : Only do this if you know what you're doing.
+impl MemFileCast for SharedState {}
+
 fn main() {
 
-    //Create a new MemFile of size 4096
+    //Create a new shared MemFile
     let mut mem_file: MemFile = match MemFile::create(PathBuf::from("test.txt"), 4096) {
         Ok(v) => v,
         Err(e) => {
@@ -14,53 +21,47 @@ fn main() {
         }
     };
 
+    //Initialize the MemFile with default values
     {
-        let mut write_buf: MemFileWLockSlice<u8> = match mem_file.wlock_as_slice() {
+        let mut shared_state: MemFileWLock<SharedState> = match mem_file.wlock() {
             Ok(v) => v,
             Err(_) => panic!("Failed to acquire write lock !"),
         };
 
-        let src = b"Hello World !\x00";
-        write_buf[0..src.len()].copy_from_slice(src);
-
-        println!("Wrote : {}", unsafe {std::str::from_utf8_unchecked(*write_buf)})
+        shared_state.num_listenners = 0;
+        let src = b"Welcome, we currently have 0 listenners !\x00";
+        shared_state.message[0..src.len()].copy_from_slice(src);
     }
-
-    println!("Waiting for first byte of shared memory to change...");
+    println!("Waiting for a listenner to connect !");
 
     //Loop until our memory has changed
     loop {
 
         //Acquire read lock
-        let read_buf: MemFileRLockSlice<u8> = match mem_file.rlock_as_slice() {
+        let shared_state: MemFileRLock<SharedState> = match mem_file.rlock() {
             Ok(v) => v,
-            Err(_) => panic!("Failed to acquire write lock !"),
+            Err(_) => panic!("Failed to acquire read lock !"),
         };
 
         //Check shared memory
-        if read_buf[0] != 0x48 {
-            println!("First byte has changed to 0x{:x} !", read_buf[0]);
-            //This will also drop the lock
+        if shared_state.num_listenners > 0 {
+            println!("We have a listenner !");
             break;
         }
 
         //Release the lock before sleeping
-        drop(read_buf);
+        drop(shared_state);
         std::thread::sleep(std::time::Duration::from_millis(200));
     }
 
-    println!("Writing some data...");
-
     //Modify the shared memory just for fun
     {
-        let mut write_buf: MemFileWLockSlice<u8> = match mem_file.wlock_as_slice() {
+        let mut shared_state: MemFileWLock<SharedState> = match mem_file.wlock() {
             Ok(v) => v,
             Err(_) => panic!("Failed to acquire write lock !"),
         };
 
-        let src = b"Bye !\x00";
-        write_buf[0..src.len()].copy_from_slice(src);
+        let src = format!("Goodbye {} listenner(s) !\x00", shared_state.num_listenners);
+        shared_state.message[0..src.len()].copy_from_slice(&src.as_bytes());
     }
-
-    println!("Done !");
 }
--- examples/open.rs
+++ examples/open.rs
@@ -3,7 +3,6 @@ use mem_file::*;
 use std::path::PathBuf;
 use std::str::from_utf8_unchecked;
 
-
 //Is there a rust function that does this ?
 fn from_ut8f_to_null(bytes: &[u8], max_len: usize) -> &str {
     for i in 0..max_len {
@@ -16,7 +15,7 @@ fn from_ut8f_to_null(bytes: &[u8], max_len: usize) -> &str {
 
 fn main() {
 
-    //Open an existing MemFile
+    //Open an existing shared MemFile
     let mut mem_file: MemFile = match MemFile::open(PathBuf::from("test.txt")) {
         Ok(v) => v,
         Err(e) => {
@@ -31,23 +30,22 @@ fn main() {
         //Acquire read lock
         let read_buf: MemFileRLockSlice<u8> = match mem_file.rlock_as_slice() {
             Ok(v) => v,
-            Err(_) => panic!("Failed to acquire write lock !"),
+            Err(_) => panic!("Failed to acquire read lock !"),
         };
 
-        print!("Orig buffer = \"");
-        print!("{}", from_ut8f_to_null(*read_buf, 256));
+        print!("Shared buffer = \"");
+        print!("{}", from_ut8f_to_null(&read_buf[4..], 256));
         println!("\"");
     }
 
-    //Write 0x01 in the first byte
+    println!("Incrementing shared listenner count !");
+    //Update the shared memory
     {
-        let mut write_buf: MemFileWLockSlice<u8> = match mem_file.wlock_as_slice() {
+        let mut num_listenners: MemFileWLock<u32> = match mem_file.wlock() {
             Ok(v) => v,
             Err(_) => panic!("Failed to acquire write lock !"),
         };
-
-        println!("write_buf[0] = 0x1");
-        write_buf[0] = 0x1;
+        *(*num_listenners) = 1;
     }
 
     //Read the contents of the buffer again
@@ -55,14 +53,11 @@ fn main() {
     {
         let read_buf = match mem_file.rlock_as_slice::<u8>() {
             Ok(v) => v,
-            Err(_) => panic!("Failed to acquire write lock !"),
+            Err(_) => panic!("Failed to acquire read lock !"),
         };
 
-        print!("After buffer = \"");
-        print!("{}", from_ut8f_to_null(*read_buf, 256));
+        print!("Shared buffer = \"");
+        print!("{}", from_ut8f_to_null(&read_buf[4..], 256));
         println!("\"");
-        println!("len 0x{:x}", read_buf.len())
     }
-
-    println!("Done");
 }
