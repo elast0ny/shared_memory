fe8b5e87dfb5d81fdf52dc414b64668038486fa6
elast0ny <elast0ny00@gmail.com>
2018-04-21 15:33:11 -0400

    Documentation changes

--- src/lock_defs.rs
+++ src/lock_defs.rs
@@ -2,12 +2,19 @@ use super::*;
 use std::ops::{Deref, DerefMut};
 use std::os::raw::c_void;
 
+#[derive(Debug)]
+///List of all possible locking mechanisms.
+///Some OS implementations might not implement all of the possible lock types in this enum.
 pub enum LockType {
+    ///Only one reader/writer can hold this lock at once
     Mutex,
+    ///Multiple readers can access the data. Writer access is exclusive.
     Rwlock,
+    ///No locking restrictions on the shared memory
     None,
 }
 
+#[doc(hidden)]
 pub struct LockNone {}
 impl MemFileLockImpl for LockNone {
     fn init(&self, _lock_data: *mut c_void) -> Result<()> {Ok(())}
@@ -37,13 +44,14 @@ impl MemFileLockImpl for LockNone {
 
 /* Lock Guards */
 
-//Read
+///Lock wrappping a non-mutable access to the shared data
 pub struct ReadLockGuard<'a, T: 'a> {
     data: &'a T,
     lock_fn: &'a MemFileLockImpl,
     lock_data: &'a mut c_void,
 }
 impl<'a, T:'a> ReadLockGuard<'a, T> {
+    #[doc(hidden)]
     pub fn lock(data_in: &'a T, lock_fn_in: &'a MemFileLockImpl, lock_data_in: &'a mut c_void) -> ReadLockGuard<'a, T> {
         //Acquire the read lock
         lock_fn_in.rlock(lock_data_in).unwrap();
@@ -64,13 +72,15 @@ impl<'a, T> Deref for ReadLockGuard<'a, T> {
     type Target = &'a T;
     fn deref(&self) -> &Self::Target { &self.data }
 }
-//Read Slice
+
+///Lock wrappping a non-mutable access to the shared data as a slice
 pub struct ReadLockGuardSlice<'a, T: 'a> {
     data: &'a [T],
     lock_fn: &'a MemFileLockImpl,
     lock_data: &'a mut c_void,
 }
 impl<'a, T:'a> ReadLockGuardSlice<'a, T> {
+    #[doc(hidden)]
     pub fn lock(data_in: &'a [T], lock_fn_in: &'a MemFileLockImpl, lock_data_in: &'a mut c_void) -> ReadLockGuardSlice<'a, T> {
         //Acquire the read lock
         lock_fn_in.rlock(lock_data_in).unwrap();
@@ -92,13 +102,14 @@ impl<'a, T> Deref for ReadLockGuardSlice<'a, T> {
     fn deref(&self) -> &Self::Target { &self.data }
 }
 
-//Write
+///Lock wrappping a mutable access to the shared data
 pub struct WriteLockGuard<'a, T: 'a> {
     data: &'a mut T,
     lock_fn: &'a MemFileLockImpl,
     lock_data: &'a mut c_void,
 }
 impl<'a, T:'a> WriteLockGuard<'a, T> {
+    #[doc(hidden)]
     pub fn lock(data_in: &'a mut T, lock_fn_in: &'a MemFileLockImpl, lock_data_in: &'a mut c_void) -> WriteLockGuard<'a, T> {
         //Acquire the write lock
         lock_fn_in.wlock(lock_data_in).unwrap();
@@ -125,13 +136,14 @@ impl<'a, T> DerefMut for WriteLockGuard<'a, T> {
     }
 }
 
-//Write Slice
+///Lock wrappping a mutable access to the shared data as a slice
 pub struct WriteLockGuardSlice<'a, T: 'a> {
     data: &'a mut [T],
     lock_fn: &'a MemFileLockImpl,
     lock_data: &'a mut c_void,
 }
 impl<'a, T:'a> WriteLockGuardSlice<'a, T> {
+    #[doc(hidden)]
     pub fn lock(data_in: &'a mut [T], lock_fn_in: &'a MemFileLockImpl, lock_data_in: &'a mut c_void) -> WriteLockGuardSlice<'a, T> {
         //Acquire the write lock
         lock_fn_in.wlock(lock_data_in).unwrap();
