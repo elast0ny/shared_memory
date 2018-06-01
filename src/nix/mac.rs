9c987b29827eb128589abdd59fe7a9faec83b806
ElasT0ny <elast0ny00@gmail.com>
2018-05-31 22:38:33 -0400

    fixes #14 implemented basic pthread_mutex_timedlock

--- src/nix/mac.rs
+++ src/nix/mac.rs
@@ -4,13 +4,44 @@ use super::libc::{
     pthread_mutex_trylock,
     EBUSY,
     ETIMEDOUT,
+    nanosleep,
+    c_int,
 };
 
 pub fn pthread_mutex_timedlock(lock: *mut pthread_mutex_t, abstime: &timespec) -> c_int {
 
     compile_error!("Must implement pthread_mutex_timedlock on macos");
 
-    //call pthread_mutex_trylock until not EBUSY with some sort of sleep to prevent busy loop
+    let mut timenow: timespec = timespec {
+        tv_sec: 0,
+        tv_nsec: 0,
+    };
 
-	ETIMEDOUT
-}
+    let timesleep: timespec = timespec {
+        tv_sec: 0,
+        tv_nsec: 10_000_000, // 10ms
+    };
+
+    let mut res: c_int;
+
+    loop {
+        res = unsafe {pthread_mutex_trylock(lock)};
+
+        if res == EBUSY {
+            // Check timeout before sleeping
+            clock_gettime(CLOCK_REALTIME, &mut timenow)
+            if timenow.tv_sec >= abstime.tv_sec && timenow.tv_nsec >= abstime->tv_nsec {
+                return ETIMEDOUT;
+            }
+
+            //Sleep for a bit
+            unsafe {nanosleep(&timesleep, null_mut())};
+
+            continue;
+        }
+
+        break;
+    }
+
+    res
+ }
