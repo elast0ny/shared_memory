207e2944b3c7891881c6b78a2f9e9e93df9351a1
elast0ny00 <elast0ny00@gmail.com>
2019-09-17 18:53:49 -0400

    Minor change to max timedlock implementation

--- src/nix/mac.rs
+++ src/nix/mac.rs
@@ -12,7 +12,7 @@ use ::libc::{
 
 use std::ptr::{null_mut};
 
-pub fn pthread_mutex_timedlock(lock: *mut pthread_mutex_t, abstime: &timespec) -> c_int {
+pub unsafe fn pthread_mutex_timedlock(lock: *mut pthread_mutex_t, abstime: &timespec) -> c_int {
 
     let mut timenow: timespec = timespec {
         tv_sec: 0,
@@ -27,17 +27,17 @@ pub fn pthread_mutex_timedlock(lock: *mut pthread_mutex_t, abstime: &timespec) -
     let mut res: c_int;
 
     loop {
-        res = unsafe {pthread_mutex_trylock(lock)};
+        res = pthread_mutex_trylock(lock);
 
         if res == EBUSY {
             // Check timeout before sleeping
-            unsafe {clock_gettime(CLOCK_REALTIME, &mut timenow)};
+            clock_gettime(CLOCK_REALTIME, &mut timenow);
             if timenow.tv_sec >= abstime.tv_sec && timenow.tv_nsec >= abstime.tv_nsec {
                 return ETIMEDOUT;
             }
 
             //Sleep for a bit
-            unsafe {nanosleep(&timesleep, null_mut())};
+            nanosleep(&timesleep, null_mut());
 
             continue;
         }
