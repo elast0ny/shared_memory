9028adddc62922df08a81a312432fa21a8558d65
elast0ny <elast0ny00@gmail.com>
2021-08-07 19:03:51 -0400

    Addressed #71 and #72

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,6 @@ use std::fs::remove_file;
 use std::path::{Path, PathBuf};
 
 use ::cfg_if::*;
-use ::fs2::FileExt;
 
 mod error;
 pub use error::*;
@@ -113,8 +112,27 @@ impl ShmemConf {
             return Err(ShmemError::MapSizeZero);
         }
 
+        // Create the mapping
+        let mapping = match self.os_id {
+            None => {
+                // Generate random ID until one works
+                loop {
+                    let cur_id = format!("/shmem_{:X}", rand::random::<u64>());
+                    match os_impl::create_mapping(&cur_id, self.size) {
+                        Err(ShmemError::MappingIdExists) => continue,
+                        Ok(m) => break m,
+                        Err(e) => {
+                            return Err(e);
+                        }
+                    };
+                }
+            }
+            Some(ref specific_id) => os_impl::create_mapping(specific_id, self.size)?,
+        };
+        debug!("Created shared memory mapping '{}'", mapping.unique_id);
+
         // Create flink
-        let fout = if let Some(ref flink_path) = self.flink_path {
+        if let Some(ref flink_path) = self.flink_path {
             debug!("Creating file link that points to mapping");
             let mut open_options: OpenOptions = OpenOptions::new();
             open_options.write(true);
@@ -126,56 +144,20 @@ impl ShmemConf {
             }
 
             match open_options.open(flink_path) {
-                Ok(f) => {
-                    debug!("Created file link '{}'", flink_path.to_string_lossy());
-                    // Lock the file asap so no one reads from it yet...
-                    if let Err(e) = f.try_lock_exclusive() {
-                        return Err(ShmemError::LinkCreateFailed(e));
+                Ok(mut f) => {
+                    // write the shmem uid asap
+                    if let Err(e) = f.write(mapping.unique_id.as_bytes()) {
+                        let _ = std::fs::remove_file(flink_path);
+                        return Err(ShmemError::LinkWriteFailed(e));
                     }
-                    Some(f)
                 }
                 Err(e) if e.kind() == ErrorKind::AlreadyExists => {
                     return Err(ShmemError::LinkExists)
                 }
                 Err(e) => return Err(ShmemError::LinkCreateFailed(e)),
             }
-        } else {
-            None
-        };
 
-        // Create the mapping
-        let mapping = match self.os_id {
-            None => {
-                // Generate random ID until one works
-                loop {
-                    let cur_id = format!("/shmem_{:X}", rand::random::<u64>());
-                    match os_impl::create_mapping(&cur_id, self.size) {
-                        Err(ShmemError::MappingIdExists) => continue,
-                        Ok(m) => break m,
-                        Err(e) => {
-                            // Do not leave the flink unpopulated
-                            if fout.is_some() {
-                                let _ = std::fs::remove_file(self.flink_path.as_ref().unwrap());
-                            }
-                            return Err(e);
-                        }
-                    };
-                }
-            }
-            Some(ref specific_id) => os_impl::create_mapping(specific_id, self.size)?,
-        };
-        debug!("Created shared memory mapping '{}'", mapping.unique_id);
-
-        // Write the os_id in the flink
-        if let Some(mut f) = fout {
-            debug!("Writing memory mapping id to flink");
-            if let Err(e) = f.write(mapping.unique_id.as_bytes()) {
-                // Do not leave the flink unpopulated
-                let _ = std::fs::remove_file(self.flink_path.as_ref().unwrap());
-
-                return Err(ShmemError::LinkWriteFailed(e));
-            }
-            let _ = f.unlock();
+            debug!("Created file link '{}' with id '{}'", flink_path.to_string_lossy(), mapping.unique_id);
         }
 
         self.owner = true;
@@ -195,54 +177,47 @@ impl ShmemConf {
             return Err(ShmemError::NoLinkOrOsId);
         }
 
-        // Get the os_id from the flink
-        if let Some(ref flink_path) = self.flink_path {
-            debug!(
-                "Open shared memory from file link {}",
-                flink_path.to_string_lossy()
-            );
-            let mut f = match File::open(flink_path) {
-                Ok(f) => {
-                    f.lock_shared().unwrap();
-                    f
-                }
-                Err(e) => return Err(ShmemError::LinkOpenFailed(e)),
-            };
-            let mut contents: Vec<u8> = Vec::new();
-            if let Err(e) = f.read_to_end(&mut contents) {
-                let _ = f.unlock();
-                return Err(ShmemError::LinkReadFailed(e));
-            }
-            let _ = f.unlock();
-
-            let link_os_id = match String::from_utf8(contents) {
-                Ok(s) => s,
-                Err(_) => return Err(ShmemError::LinkDoesNotExist),
-            };
-            if let Some(os_id) = self.os_id.as_ref() {
-                if *os_id != link_os_id {
-                    return Err(ShmemError::FlinkInvalidOsId);
-                }
+        let mut flink_uid = String::new();
+        let mut retry = 0;
+        loop {
+            let unique_id = if let Some(ref unique_id) = self.os_id {
+                unique_id.as_str()
             } else {
-                self.os_id = Some(link_os_id);
+                let flink_path = self.flink_path.as_ref().unwrap();
+                debug!(
+                    "Open shared memory from file link {}",
+                    flink_path.to_string_lossy()
+                );
+                let mut f = match File::open(flink_path) {
+                    Ok(f) => f,
+                    Err(e) => return Err(ShmemError::LinkOpenFailed(e)),
+                };
+                flink_uid.clear();
+                if let Err(e) = f.read_to_string(&mut flink_uid) {
+                    return Err(ShmemError::LinkReadFailed(e));
+                }
+                flink_uid.as_str()
+            };
+
+            match os_impl::open_mapping(&unique_id, self.size) {
+                Ok(m) => {
+                    self.size = m.map_size;
+                    self.owner = false;
+
+                    return Ok(Shmem {
+                        config: self,
+                        mapping: m,
+                    });
+                },
+                // If we got this failing os_id from the flink, try again in case the shmem owner didnt write the full
+                // unique_id to the file
+                Err(ShmemError::MapOpenFailed(_)) if self.os_id.is_none() && retry < 5 => {
+                    retry += 1;
+                    std::thread::sleep(std::time::Duration::from_millis(50));
+                }
+                Err(e) => return Err(e),
             }
         }
-
-        let os_id = match self.os_id.as_ref() {
-            Some(i) => i,
-            None => return Err(ShmemError::NoLinkOrOsId),
-        };
-
-        debug!("Openning shared memory id {}", os_id);
-        let mapping = os_impl::open_mapping(os_id, self.size)?;
-
-        self.size = mapping.map_size;
-        self.owner = false;
-
-        Ok(Shmem {
-            config: self,
-            mapping,
-        })
     }
 }
 
